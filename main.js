c27c29e06b534aeb6f3fd9017c0dc07255e4a3ea
Yorman Amaro <amarocarrero24@gmail.com>
2022-09-05 18:32:28 -0300

    Class 5 of Intermediet POO

---
+++ main.js
@@ -0,0 +1,21 @@
+const juan = {
+    name: "juannito",
+    age: 18,
+    approvedCourses: ["Curso1"],
+    addCourses(newCourse) {      
+        console.log("This", this);
+        console.log("This.approveCourses", this.approvedCourses);        // Se crea un metodo que recibe un parametro
+        this.approvedCourses.push(newCourse);                           // va a llamar a this que es nuestro objeto juan
+    }
+};
+
+
+//console.log(Object.keys(juan));
+//console.log(Object.getOwnPropertyNames(juan));
+//console.log(Object.entries(juan)); 
+
+
+//Object.seal(juan);
+Object.freeze(juan);
+
+console.log(Object.getOwnPropertyDescriptors(juan));
